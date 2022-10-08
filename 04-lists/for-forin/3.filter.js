0f1990e92108d682b7b415b659e54459aba9390a
Victor Pagnozi <victor.pagnozi@gmail.com>
2022-10-08 18:50:33 -0300

    Created method filters

---
+++ 04-lists/for-forin/3.filter.js
@@ -0,0 +1,18 @@
+const { obterPessoas } = require("./service");
+
+async function main() {
+    try {
+        const { results } = await obterPessoas("a");
+
+        const familiaLars = results.filter(
+            (item) => item.name.toLowerCase().indexOf("lars") !== -1
+        );
+
+        const names = familiaLars.map(pessoa => pessoa.name)
+        console.log(names);
+    } catch (error) {
+        console.error("erro", error);
+    }
+}
+
+main();
